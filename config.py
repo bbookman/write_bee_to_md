f336f938fbdb76429bb5d011236e1fcdff6f6a08
Bruce Bookman <bruce.bookman@gmail.com>
2025-05-07 12:10:42 -0400

    reduced rank

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ BEE_API_KEY = ""
 BEE_API_ENDPOINT = "https://api.bee.computer/v1"
 TARGET_DIR = "/Users/brucebookman/bee_to_md"
 FACTS_FILE_PATH = "/Users/brucebookman/bee_test_bee_to_md/facts.md"
-TEXT_RANK_COMPRESSION_RATIO = 0.7  # Set to 0.7 by default (keep 70% of content)
+TEXT_RANK_COMPRESSION_RATIO = 0.3  # Set to 0.7 by default (keep 70% of content)
